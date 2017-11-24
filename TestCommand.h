1a2972a6ef2766700bc59de27138983da2dbde8b
Artem Antonov <aantonov.develop@gmail.com>
2017-11-25 02:31:59 +0500

    Add unit tests for Command class

---
+++ TestCommand.h
@@ -0,0 +1,22 @@
+//
+// Created by artem on 25.11.17.
+//
+
+#ifndef HACKATON_WALLET_TESTCOMMAND_H
+#define HACKATON_WALLET_TESTCOMMAND_H
+
+#include "command.h"
+
+#include <QtTest/QtTest>
+#include <QStringList>
+
+class TestCommand: public QObject {
+    Q_OBJECT
+
+private slots:
+    void toJson();
+    void toString();
+};
+
+
+#endif //HACKATON_WALLET_TESTCOMMAND_H
